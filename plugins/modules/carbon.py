db1c2354c37d999474192be284b5cae9f2b77775
Shivaprasad <91235488+Ssfive123@users.noreply.github.com>
2022-05-02 10:26:12 +0530

    Update carbon.py

--- plugins/modules/carbon.py
+++ plugins/modules/carbon.py
@@ -5,7 +5,7 @@ from plugins.function import make_carbon
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 aiohttpsession = ClientSession()
 
-C = "**Made by @The_obanai_bot**"
+C = "**Made by @Spidey_Autofilterbot**"
 F = InlineKeyboardMarkup(
 [[
      InlineKeyboardButton("Add Link", url="https://t.me/telegram")
