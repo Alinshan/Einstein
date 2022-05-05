da6558cee0abcc1bf272542a77d4ae40ca2018d8
ERROR X PREDATOR <86637047+Alinshan@users.noreply.github.com>
2022-05-05 13:30:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Einstein
 fi
-cd /Ajax
+cd /Einstein
 pip3 install -U -r requirements.txt
 echo "Starting Einstein....🔥"
 python3 bot.py
